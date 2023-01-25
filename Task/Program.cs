85c07e3dfe6f9e39c94325c2250ffc96aded5a50
bionikum <bionikum@yandex.ru>
2023-01-25 17:41:22 +0500

    Algorithm

---
+++ Task/Program.cs
@@ -0,0 +1,24 @@
+﻿string[] arrayInitial = new string[7] {"^@^", "45", "hello", "task", "yes", "378", "/*"};
+string[] arrayFinal = new string[arrayInitial.Length];
+void InputArray(string[] arrayInitial, string[] arrayFinal)
+{
+    int count = 0;
+    for (int i = 0; i < arrayInitial.Length; i++)
+    {
+    if(arrayInitial[i].Length <= 3)
+        {
+        arrayFinal[count] = arrayInitial[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}  ");
+    }
+    Console.WriteLine();
+}
+InputArray(arrayInitial, arrayFinal);
+PrintArray(arrayFinal);
